e6d6f77c6deaeffbfc888146f6f32817e699e55b
albearli <yiliang6@illinois.edu>
2020-12-02 00:03:20 -0600

    Update emulator to produce realistic usernames

--- server/emulator.py
+++ server/emulator.py
@@ -17,37 +17,32 @@ def convertUsersToSet(allUsers):
 
     return userSet
 
-def addRandomUsers(len, num, currUsers):
+def addRandomUsers(num, currUsers):
     i = 0
     fails = 0
-    while (i < num):
-        # https://www.askpython.com/python/examples/generate-random-strings-in-python
-        toAdd = ""
-        for _ in range(len):
-            # Considering only upper and lowercase letters
-            random_integer = random.randint(97, 97 + 26 - 1)
-            flip_bit = random.randint(0, 1)
-            # Convert to lowercase if the flip bit is on
-            random_integer = random_integer - 32 if flip_bit == 1 else random_integer
-            # Keep appending random characters using chr(x)
-            toAdd += (chr(random_integer))
-        if (toAdd not in currUsers):
-            i += 1;
-            requests.post(CREATE_USER, data = {'Username' : toAdd, 'Name' : names.get_full_name(), 'Description' : 'I love Tangram! It is the best!'})
-            currUsers.add(toAdd)
-            print(toAdd + ' added to DB\n')
+    while i < num:
+        toAdd = names.get_full_name()
+        firstName = toAdd.split()[0]
+        if (firstName not in currUsers):
+            i += 1
+            requests.post(CREATE_USER, json={'Username' : firstName, 'Name' : toAdd, 'Description' : 'I love Tangram! It is the best!'})
+            currUsers.add(firstName)
+            print(firstName + ' added to DB\n')
         else:
             fails += 1
-            if (fails >= 100000):
+            if fails >= 100000:
                 print("Pick a bigger len")
                 break
         
 
-#r = requests.post('http://localhost:5000/create-user', data={ 'Username' : 'Albert' })
+# requests.post(CREATE_USER, json = {'Username' : 'TheodoreSpeaks', 'Name' : 'Teddy Li', 'Description' : 'I like CS and Bees'})
+# requests.post(CREATE_USER, json = {'Username' : 'Albear', 'Name' : 'Albert Li', 'Description' : 'Our database got hacked'})
+# requests.post(CREATE_USER, json = {'Username' : 'Mdpham', 'Name' : 'Matthew Pham', 'Description' : 'ECE for life'})
+# requests.post(CREATE_USER, json = {'Username' : 'Aritrooo', 'Name' : 'Aritro Nandi', 'Description' : 'Epic'})
 
 x = requests.get(GET_ALL_USERS)
 s = convertUsersToSet(x)
 
-addRandomUsers(4, 20, s)
+addRandomUsers(20, s)
 
 print(requests.get(GET_ALL_USERS).text)
